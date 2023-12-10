78e66b6b20a9fb232e59d945ed28aed1bb149e28
endrikpartin <endrik-sander.partin@voco.ee>
2023-12-10 15:41:43 +0200

    K6ik ylesanded

---
+++ Desktop/Powershell/yl12.ps1
@@ -0,0 +1,34 @@
+﻿#Esimene kysimus
+function countstart
+{
+$start = Get-Service | ?{$_.Status -eq "Running"}
+Write-Host "Total services in running state = "$start.count
+}
+function countstop
+{
+$stop = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-Host "Total services in stopped state = "$stop.count
+}
+countstart
+countstop
+
+#Teine kysimus
+function countservice
+{
+param
+(
+[string]$status
+)
+if($status -eq "Running")
+{
+$start = Get-Service | ?{$_.Status -eq "Running"}
+Write-Host "Total services in running state = "$start.count
+}
+elseif($status -eq "Stopped")
+{
+$stop = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-Host "Total services in stopped state = "$stop.count
+}
+}
+countservice -status Running
+countservice -status Stopped
